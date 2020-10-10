8ebe97c85281771a0a891c558195a55fd8b98a12
Tomasz Dudkowski <tomaszszd@gmail.com>
2020-10-10 22:36:09 +0200

    Added vertical menu for panel site. Added client model.

---
+++ InvoiceCatWebApp/Controllers/Panel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceCatWebApp.Controllers
+{
+    public class Panel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InvoiceCatWebApp/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceCatWebApp.Models
+{
+    public class Client
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
