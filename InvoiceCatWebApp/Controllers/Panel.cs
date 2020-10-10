ee3e1b1c25701701720e2acafc754f649e903ead
Tomasz Dudkowski <tomaszszd@gmail.com>
2020-10-11 00:10:41 +0200

    Added partial view for menu and update ApplicationDbContext.

--- InvoiceCatWebApp/Controllers/Panel.cs
+++ InvoiceCatWebApp/Controllers/Panel.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InvoiceCatWebApp.Data;
+using InvoiceCatWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace InvoiceCatWebApp.Controllers
 {
     public class Panel : Controller
     {
+
+        private readonly ILogger<Panel> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public Panel(ApplicationDbContext context, ILogger<Panel> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Clients()
+        {
+            var clients = _context.Clients;
+
+            return View(clients);
         }
     }
 }
---
+++ InvoiceCatWebApp/Data/Migrations/20201010210209_DbSetClients.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InvoiceCatWebApp.Data.Migrations
+{
+    public partial class DbSetClients : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Surname = table.Column<string>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
